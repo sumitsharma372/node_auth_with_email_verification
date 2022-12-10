1c62b901db902b92e0ecdd99e71d0a0b0137d8ec
sumitsharma372 <sksharma372@kgpian.iitkgp.ac.in>
2022-12-11 03:07:47 +0530

    updated

---
+++ client/src/components/emailVerify/index.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { Link,useParams } from 'react-router-dom'
+import success from '../../images/success.svg'
+import styles from './styles.module.css'
+
+const EmailVerify = () => {
+    const [validUrl, setValidUrl] = useState(false)
+    const params = useParams()
+
+    useEffect(() => {
+        const verifyEmailUrl = async () => {
+            try {
+                const url = `http://localhost:8080/api/users/${params.id}/verify/${params.token}`
+                const { data } = await axios.get(url)
+                console.log(data)
+                setValidUrl(true)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        verifyEmailUrl()
+    }, [params])
+
+  return (
+    <>
+        {validUrl ? (
+            <div className={styles.container}>
+                <img src={success} alt="success_img" className={styles.success_img}/>
+                <Link to='/login'>
+                    <button className={styles.green_btn}>
+                        Login
+                    </button>
+                </Link>
+            </div>
+        ) : (
+            <h1>404 not found</h1>
+        )}
+    </>
+  )
+}
+
+export default EmailVerify
---
+++ client/src/components/login/index.jsx
@@ -0,0 +1,76 @@
+import styles from './styles.module.css';
+import { Link  } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [error, setError] = useState("");
+
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = async(e) => {
+            e.preventDefault();
+            try {
+                const url = "http://localhost:8080/api/users/login";
+                const { data : res } = await axios.post(url, data)
+                localStorage.setItem("token", JSON.stringify(res));
+                window.location.href = "/";
+            } catch (error) {
+                if(error.response && error.response.status >= 400 && error.response.status < 500) {
+                    setError(error.response.data.message);
+                }
+
+            }
+        }
+
+    return (
+        <div className={styles.login_container}>
+            <div className={styles.login_form_container}>
+                <div className={styles.left}>
+                    <form className={styles.form_container} onSubmit = {handleSubmit}>
+                        <h1>Login</h1>
+                        <input 
+                            type="email" 
+                            placeholder='Email'
+                            name="email"
+                            value = {data.email}
+                            required
+                            className={styles.input}
+                            onChange = {handleChange}
+                        />
+                        <input 
+                            type="password" 
+                            placeholder='Password'
+                            name="password"
+                            value = {data.password}
+                            required
+                            className={styles.input}
+                            onChange = {handleChange}
+                        />
+                        {error && <div className={styles.error_msg}>{error}</div>}
+                        <button type='submit' className={styles.green_btn}>
+                            Sign In
+                        </button>
+                    </form>
+                </div>
+                <div className={styles.right}>
+                    <h1>Don't have an account ?</h1>
+                    <Link to = '/signup'>  
+                        <button type='button' className={styles.white_btn}>
+                            Sign up
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login; 
