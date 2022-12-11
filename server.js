88dc416951de45b67345bc5f25948542fe47439e
sumitsharma372 <sksharma372@kgpian.iitkgp.ac.in>
2022-12-11 10:49:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,3 +27,10 @@ mongoose.connect(process.env.MONGO_URI)
         console.error(err)
     }
 )
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+    const path = require('path')
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
